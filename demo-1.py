e37581dbda14b7472a80eb714dc58ff071887971
goforbroke <go.for.broke1006@gmail.com>
2018-01-07 23:18:03 +0300

    Detect face parts

--- demo-1.py
+++ demo-1.py
@@ -5,7 +5,7 @@ import dlib
 import cv2
 from imutils import face_utils
 
-img_input = np.array(Image.open('./samples/images/4.jpg'))
+img_input = np.array(Image.open('./samples/images/2.jpg'))
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('./shape_predictor_68_face_landmarks.dat')
 img_gray = cv2.cvtColor(img_input, cv2.COLOR_BGR2GRAY)
@@ -16,8 +16,32 @@ shape = predictor(img_gray, rects[0])
 shape = face_utils.shape_to_np(shape)
 
 img_tmp = img_input.copy()
+index = 0
 for x, y in shape:
-    cv2.circle(img_tmp, (x, y), 1, (0, 0, 255), -1)
-plt.imshow(img_tmp)
+    if 0 <= index <= 16:
+        ff_color = (0, 255, 0) # face contour
+    elif 17 <= index <= 21:
+        ff_color = (0, 255, 255) # right eyebrow
+    elif 22 <= index <= 26:
+        ff_color = (255, 0, 0) # left eyebrow
+    elif 27 <= index <= 30:
+        ff_color = (255, 0, 255) # nose center line
+    elif 31 <= index <= 35:
+        ff_color = (128, 255, 128) # nose bottom line
+    elif 36 <= index <= 41:
+        ff_color = (255, 64, 128) # right eye
+    elif 42 <= index <= 47:
+        ff_color = (128, 64, 255) # left eye
+    elif 48 <= index <= 54:
+        ff_color = (0, 255, 255) # lips (top line)
+    elif 55 <= index <= 59:
+        ff_color = (128, 255, 128) # lips (bottom line)
+    else:
+        ff_color = (255, 255, 0) # lips
+
+    cv2.circle(img_tmp, (x, y), 3, ff_color, -1)
+    plt.imshow(img_tmp)
+
+    index = index + 1
 
 plt.show()
